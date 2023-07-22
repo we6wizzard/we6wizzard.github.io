7e6e26eb8cdf7b1faa5618ec1a89e1538501dc0d
we6wizzard <charlene_rees11@hotmail.com>
2023-07-22 15:59:59 +0100

    JS ex1, index page editing, first exerises/projects of frontend masters course

---
+++ index1.js
@@ -0,0 +1,10 @@
+
+
+
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + "=^.^=");
+    }
+};
+
+drawCats(10); //You can put any number here instead of 10.
