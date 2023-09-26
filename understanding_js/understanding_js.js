7e38d790d9ce301273815e7a64946ebe5e0e12f2
Darren Rees <d.syph.3r@gmail.com>
2023-09-26 16:34:11 +0100

    updates for mouse over and mouse out

---
+++ understanding_js/understanding_js.js
@@ -0,0 +1,9 @@
+document.write(Date());
+
+function setNewImage() {
+    document.getElementById("img1").src="images/mouse-over.png";
+}
+
+function setOldImage() {
+    document.getElementById("img1").src="images/mouse-out.png";
+}
