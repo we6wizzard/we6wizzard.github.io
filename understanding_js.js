d6a0da992ac2a0a971ff8d335fc32570a2a595cb
we6wizzard <charlene_rees11@hotmail.com>
2023-09-26 16:04:51 +0100

    Learning JS

---
+++ understanding_js.js
@@ -0,0 +1,18 @@
+document.write(Date());
+
+
+
+
+
+function setNewImage() {
+    document.getElementById("img1").src="C:\Users\charl\OneDrive\Mobile uploads\Pictures\bird.jpg";
+    
+
+
+}
+
+function setOldImage() {
+    document.getElementById("img1").src="C:\Users\charl\OneDrive\Mobile uploads\Pictures\simple_snippets.png";
+    
+
+}
