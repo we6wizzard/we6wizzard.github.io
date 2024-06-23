c77ad64d94ef4aec22b970233ee936dde6a36c62
we6wizzard <130064767+we6wizzard@users.noreply.github.com>
2024-06-23 22:53:05 +0100

    Dice Game and Drum Kit Files and Exercises

--- UDEMY COMPLETE WEB DEV BOOTCAMP PROJECTS/Dice Game/index.js
+++ UDEMY COMPLETE WEB DEV BOOTCAMP PROJECTS/Dice Game/index.js
@@ -10,9 +10,17 @@ let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML="Player 1 Wins";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
 }
-else if ()
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML= "Player 2 Wins! 🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
+
 
 
 
