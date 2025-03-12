c7cdfd8d433a39b45d1f4bcca6b926bce43cccb1
we6wizzard <130064767+we6wizzard@users.noreply.github.com>
2025-03-12 00:47:27 +0000

    Restored Calc Apps

--- JAVASCRIPT/CALCULATOR 2/index.js
+++ JAVASCRIPT/CALCULATOR 2/index.js
@@ -1,48 +1,123 @@
-let count = 0;
+let count = "";
 
-let decreaseButton = document.querySelector(".decrease");
-let increaseButton = document.querySelector(".increase");
-let decreaseButton2 = document.querySelector(".decrease2");
-let increaseButton2 = document.querySelector(".increase2")
-console.log(decreaseButton);
-console.log(increaseButton);
-console.log(increaseButton2);
-console.log(decreaseButton2);
+const display = document.getElementById("display");
 
-let counterInput = document.querySelector(".counter");
-console.log(counterInput);
 
-updateCounterInputValue();
+// * 10
 
-decreaseButton.addEventListener("click", function () {
-  updateCount(-1);
-});
 
-decreaseButton2.addEventListener("click", function () {
-  updateCount(-2);
-});
+const multiplyButton10 = document.getElementById("multiply");
 
-increaseButton2.addEventListener("click", function () {
-  updateCount(2);
+function multiply10 () {
+  let currentValue = parseInt(display.value);
+  currentValue *=10;
+  display.value = currentValue;
+}
 
-});
 
-increaseButton.addEventListener("click", function () {
-  updateCount(1);
-})
+multiplyButton10.addEventListener("click", multiply10);
 
-function updateCounterInputValue() {
-  counterInput.value = count;
+
+
+// +2 
+
+const increaseButton2 = document.getElementById("increase2");
+
+function increaseBy2() {
+  let currentValue = parseInt(display.value);
+  currentValue += 2;
+  display.value = currentValue;
+}
+
+increaseButton2.addEventListener("click", increaseBy2);
+
+
+//+5
+
+
+const increaseButton5 = document.getElementById("increase5");
+
+function increaseBy5() {
+  let currentValue = parseInt(display.value);
+  currentValue +=5;
+  display.value = currentValue
 
 }
 
-function updateCount(updateBy) {
-  let newCount = count + updateBy;
+increaseButton5.addEventListener("click", increaseBy5);
 
-  if (newCount >= 0 && newCount <=9) {
-     count = newCount;
 
-  }
+// +
 
-  updateCounterInputValue();
+const increaseButton1 = document.getElementById("increase");
+
+function increase (){
+let currentValue = parseInt(display.value);
+currentValue +=1;
+display.value = currentValue;
+
+
 }
+
+increaseButton1.addEventListener("click", increase);
+
+
+
+// -
+
+const decreaseButton1 = document.getElementById("decrease");
+
+function decrease() {
+  let currentValue = parseInt(display.value);
+  currentValue -=1;
+  display.value = currentValue;
+}
+
+
+decreaseButton1.addEventListener("click", decrease);
+
+function clearScreen () {
+  decreaseButton1.getElementById("display").value = "";
+}
+
+
+// -5
+
+
+const decreaseButton5 = document.getElementById("decrease5");
+
+function decreaseBy5() {
+  let currentValue = parseInt(display.value);
+  currentValue -=5;
+  display.value = currentValue
+}
+
+decreaseButton5.addEventListener("click", decreaseBy5);
+
+
+
+
+//-2
+
+const decreaseButton2 = document.getElementById("decrease2");
+
+function decreaseBy2() {
+  let currentValue = parseInt(display.value);
+  currentValue -=2
+  display.value = currentValue;
+
+}
+
+decreaseButton2.addEventListener("click", decreaseBy2);
+
+
+
+
+
+
+
+
+
+
+
+
