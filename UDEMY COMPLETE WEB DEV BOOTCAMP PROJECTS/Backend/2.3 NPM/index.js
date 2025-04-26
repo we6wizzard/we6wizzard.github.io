05f858848b0729b20d50a89ffbb2cde319dddd41
we6wizzard <130064767+we6wizzard@users.noreply.github.com>
2025-04-26 21:41:50 +0100

    Learning node + Express

---
+++ UDEMY COMPLETE WEB DEV BOOTCAMP PROJECTS/Backend/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+eeeeee
+
+//import generateName from "sillyName";
+//let sillyName = generateName();
+
+//console.log(`My Name Is ${sillyName}. `);
+
+
+
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
