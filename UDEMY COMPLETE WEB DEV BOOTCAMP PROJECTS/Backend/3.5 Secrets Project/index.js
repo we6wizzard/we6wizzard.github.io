f088fe1c7cf0ec364f1d51cda747d7e3640f0c7d
we6wizzard <130064767+we6wizzard@users.noreply.github.com>
2025-05-04 14:53:41 +0100

    Lesson 207 - Secrets Access Project Exercise + Starting New Section - Section 25: EJS: Lesson 209

---
+++ UDEMY COMPLETE WEB DEV BOOTCAMP PROJECTS/Backend/3.5 Secrets Project/index.js
@@ -0,0 +1,36 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+let userAuthenticated = false;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+function passwordCheck(req,res,next) {
+    const password = req.body["password"];
+    if (password === "ILoveProgramming") {
+        userAuthenticated = true;
+    }
+
+    next();
+
+}
+
+app.use(passwordCheck);
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/public/index.html");
+
+});
---
+++ UDEMY COMPLETE WEB DEV BOOTCAMP PROJECTS/Backend/4.0 EJS/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+const app=express(); //Setting node's framework as Express
+const port=3000;
+
+app.set('view engine', 'ejs'); //Setting EJS as the template engine
+
+//Advice for each day (Sunday = 0)
+
+const adviceByDay = [
+    "Take it easy - Sunday is for rest.",
+    "Monday: Start the week strong! Set your goals.",
+    "Tuesday: Keep pushing! You're doing great.",
+    "Wednesday: Midweek hump - stay focused!",
+    "Thursday: Almost there - finish strong!",
+    "It's Fri-YAY! Reflect and prepare to relax.",
+    "Saturday: Recharge and get ready for the week ahead"
+];
+
+app.get('/', (req, res) => {
+    const day = new Date().getDay();
+    const advice = adviceByDay[day];
+    res.render('index.ejs', {advice}); //pass data from our server over to
+    //our template file, the ejs file. the ejs file has to live in a 
+    //"views" folder and we use the ejs syntax to pick up those properties and insert them into our html.
+
+});
+
+app.listen(port, () => {
+    console.log(`Server Is Running on http://localhost:${port}`);
+});
